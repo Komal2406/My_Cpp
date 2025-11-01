adf4748de1e0a6b82de439dac4246330130f38c0
Komal2406 <kaurkomalpreet2405@gmail.com>
2025-11-01 20:11:07 +0530

    Add new commit

--- 01_question2.cpp
+++ 01_question2.cpp
@@ -2,7 +2,8 @@
 #include<iomanip>
 using namespace std;
 
-int main(){
+//Int main is starting
+int main(){      
     int a1 = 135, a2 =7290, a3 = 11300, a4 = 16200;
     cout<<setw(8)<<"1990"<<setw(12)<<a1<<endl
         <<setw(8)<<"1991"<<setw(12)<<a2<<endl
